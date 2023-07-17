58686acea8ba461c9c8ce9d8ef31cf241626d0ea
Abhi Pandey <pandeyabhi142002@gmail.com>
2023-07-17 17:18:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useEffect, useState } from 'react'
 import './../styles/App.css';
 
+
 const App = () => {
+  const [sec, setSec] = useState(0);
+
+    useEffect(() => {
+        let intervalId = setInterval(() => {
+            setSec(prev => prev+1);
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, []);
+  
   return (
     <div>
         {/* Do not remove the main div */}
+    You've been on this page for {sec} seconds.
     </div>
   )
 }
